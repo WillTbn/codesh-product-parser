02e947be640156e12137ca89e5bc3ff886cd29c0
JorgeNunes <jlbnunes@live.com>
2024-08-26 00:55:26 -0300

    feat:get memory usage, process duration - and walk through all .gz files

--- app/Services/OpenFoodFacts/ImportOFFServices.php
+++ app/Services/OpenFoodFacts/ImportOFFServices.php
@@ -28,10 +28,28 @@ class ImportOFFServices extends Service
     /**
      * limit
      */
-    public ?int $limit = 50;
-
+    public ?int $limit = 100;
+    /**
+     * variable with datas product
+     */
     public array $productData = [];
 
+    /**
+     * variable with initial date process
+     */
+    public float|int $duration_process;
+    /**
+     * variable initial memory
+     */
+    public string $memory_initial;
+    /**
+     * variable memory finally
+     */
+    public string $memory_finally;
+    /**
+     * total memory usage
+     */
+    public string $memory_usage;
     /**
      * set FilesName
      */
@@ -75,7 +93,25 @@ class ImportOFFServices extends Service
 
         $this->productData[] = Arr::add(  $this->productData, 'status', Arr::random(ProductStatus::forSelectName()));
     }
-
+    /**
+     * set duration process
+     */
+    public function setDurationProcess(float|int $seconds)
+    {
+        $this->duration_process = number_format($seconds, 2);
+    }
+    public function setMemoryInitial(float|int $startMemory)
+    {
+        $this->memory_initial = $this->formatBytes($startMemory);
+    }
+    public function setMemoryFinally(float|int $endMemory)
+    {
+        $this->memory_finally = $this->formatBytes($endMemory);
+    }
+    public function setMemoryUsage(float|int $peakMemory)
+    {
+        $this->memory_usage = $this->formatBytes($peakMemory);
+    }
     /**
      * get filesName
      * @return array
@@ -94,6 +130,22 @@ class ImportOFFServices extends Service
     {
         return $this->productData;
     }
+    public function getDurationProcess()
+    {
+        return $this->duration_process;
+    }
+    public function getMemoryInitial()
+    {
+        return $this->memory_initial;
+    }
+    public function getMemoryFinally()
+    {
+        return $this->memory_finally;
+    }
+    public function getMemoryUsage()
+    {
+        return $this->memory_usage;
+    }
     /**
      *
      */
@@ -101,67 +153,98 @@ class ImportOFFServices extends Service
     {
 
     }
-    public function processLargeGz()
+    public function processLarge()
     {
+         // Início do processo
+        $startTime = microtime(true);
+        $this->setMemoryInitial(memory_get_usage());
         $tempFilePath = storage_path('app/temp_file.gz');
-        $this->setFilePath('products_01.json.gz');
-
-        // Baixando o arquivo da URL
-        $response = Http::get($this->filePath);
-
-        if ($response->successful()) {
-            // Salvando o conteúdo do arquivo compactado
-            file_put_contents($tempFilePath, $response->body());
-
-            // Abrindo o arquivo .gz como um stream
-            $gz = gzopen($tempFilePath, 'rb');
-
-            $productCount = 0;
-            $buffer = '';
-
-            // Processando linha a linha
-            while (!gzeof($gz)) {
-                $line = gzgets($gz);
-
-                // Adicionando a linha ao buffer
-                $buffer .= $line;
-
-                // Tentando decodificar o JSON no buffer
-                $jsonProduct = json_decode($buffer, true);
-
-                // Se decodificou com sucesso, processamos o produto
-                if (json_last_error() === JSON_ERROR_NONE) {
-                    // $product =json_encode($jsonProduct) ;
-                    // Log::info('Produto: '.$jsonProduct['code']);
-                    $this->setProductData($jsonProduct);
-
-                    Log::info('Produto: '.json_encode($this->getProductData()));
-                    $productCount++;
-
-                    if ($productCount >= $this->limit) {
-                        Log::info('Limite atingido: ' . $productCount);
-                        break;
-                    }
-
-                    // Resetando o buffer para o próximo produto
-                    $buffer = '';
-                }
+        foreach($this->getFilesName() as $filename){
+            $this->setFilePath($filename);
+            // Baixando o arquivo da URL
+            $response = Http::get($this->filePath);
+            if(!$response->successful()){
+                throw new \Exception('Falha ao baixar o arquivo da URL: ' . $this->filePath);
             }
+            if ($response->successful()) {
+                // Salvando o conteúdo do arquivo compactado
+                file_put_contents($tempFilePath, $response->body());
 
-            Log::info('Processamento concluído com ' . $productCount . ' produtos.');
+                // Abrindo o arquivo .gz como um stream
+                $gz = gzopen($tempFilePath, 'rb');
 
-            // Fechando o stream
-            gzclose($gz);
+                $productCount = 0;
+                $buffer = '';
 
-            // Removendo arquivo temporário
-            unlink($tempFilePath);
-        } else {
-            throw new \Exception('Falha ao baixar o arquivo da URL: ' . $this->filePath);
+                // Processando linha a linha
+                while (!gzeof($gz)) {
+                    $line = gzgets($gz);
+
+                    // Adicionando a linha ao buffer
+                    $buffer .= $line;
+
+                    // Tentando decodificar o JSON no buffer
+                    $jsonProduct = json_decode($buffer, true);
+
+                    // Se decodificou com sucesso, processamos o produto
+                    if (json_last_error() === JSON_ERROR_NONE) {
+                        // $product =json_encode($jsonProduct) ;
+                        // Log::info('Produto: '.$jsonProduct['code']);
+                        $this->setProductData($jsonProduct);
+
+                        Log::info('Produto: '.json_encode($this->getProductData()));
+                        $productCount++;
+
+                        if ($productCount >= $this->limit) {
+                            Log::info('Limite atingido: ' . $productCount);
+                            break;
+                        }
+
+                        // Resetando o buffer para o próximo produto
+                        $buffer = '';
+                    }
+                }
+
+                Log::info('Processamento concluído com ' . $productCount . ' produtos.');
+
+                // Fechando o stream
+                gzclose($gz);
+
+                // Removendo arquivo temporário
+                unlink($tempFilePath);
+            }
         }
+        // Fim do processo
+        $endTime = microtime(true);
+        $this->setMemoryFinally(memory_get_usage());
+        $this->setMemoryUsage(memory_get_peak_usage());
+
+        // Calculando o tempo total do processo
+        $this->setDurationProcess( $endTime - $startTime);
+
+
+        Log::info('Processamento concluído com ' . $productCount . ' produtos.');
+        Log::info('Duração do processo: ' . $this->getDurationProcess() . ' segundos');
+        Log::info('Memória inicial: ' .$this->getMemoryInitial());
+        Log::info('Memória final: ' . $this->getMemoryFinally());
+        Log::info('Pico de uso de memória: ' . $this->getMemoryUsage());
+    }
+
+    private function formatBytes($bytes, $precision = 2)
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
     }
     public function execute()
     {
-        $this->processLargeGz();
+        $this->processLarge();
         return $this;
     }
 }
